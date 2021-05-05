4b3b0a4440951709d7cd138dae31241012b91342
shauna.s.peterson@hotmail.com <shauna.s.peterson@hotmail.com>
2021-05-05 18:24:10 -0400

    still working :/

--- cart.js
+++ cart.js
@@ -23,3 +23,47 @@
 
 // module.exports = cart;
 
+const express = require('express');
+
+const cart = express.Router();
+cart.use(express.json());
+
+const cartItems =[
+    {
+        id: 1,
+        product: 'Chili',
+        price: 27.00,
+        quantity: 15,
+    },
+
+    {
+        id:2,
+        product: 'Bananas',
+        price:80.00,
+        quantity: 1,
+    },
+
+    {
+        id: 3,
+        product:'Wine',
+        price:100.00,
+        quantity:1,
+    },
+
+    {   
+        id: 4,
+        product: 'Cookies',
+        price: 79.00,
+        quantity: 5,
+
+    },
+];
+
+
+cart.get('/cart-items', (req, res) => {
+    for(carts of cartItems);
+    res.json(carts[0]);
+    
+});
+
+module.exports = cart;
--- server.js
+++ server.js
@@ -7,54 +7,111 @@
 // const port = 3000;
 // app.listen(port, () => console.log(`Listening on port: ${port}.`));
 
-const { Router } = require('express');
+// const { Router } = require('express');
+// const express = require('express');
+// const app = express();
+// const port = 3000;
+
+// app.use = (express.json);
+
+// const cartItems =[
+//     {
+//         id: 1,
+//         product: 'Chili',
+//         price: 27.00,
+//         quantity: 15,
+//     },
+
+//     {
+//         id:2,
+//         product: 'Bananas',
+//         price:80.00,
+//         quantity: 1,
+//     },
+
+//     {
+//         id: 3,
+//         product:'Wine',
+//         price:100.00,
+//         quantity:1,
+//     },
+
+//     {   
+//         id: 4,
+//         product: 'Cookies',
+//         price: 79.00,
+//         quantity: 5,
+
+//     },
+// ];
+// app.get('/cart-items', (req, res) => {
+//     res.json(cartItems[0])
+// });
+
+// router.post("/cart-items", (req, res) => {
+//     console.log(req.body)
+//     cartItems.push({
+//         id: cartItems, length + 1,
+//         product: req.body.product,
+//         price:req.body.price,
+//         quantity: req.body.quantity,
+//     })
+// })
+
+// app.listen(port, () => {
+//     console.log(`Example app listening at http://localhost:${port}`)
+// });
+
+// module.exports = router;
+
 const express = require('express');
-const router = express();
+const cart = require('./cart');
+const app = express();
+// app.use('/', cart());
 const port = 3000;
+
+
+const carts = express.Router();
+cart.use(express.json());
+
 const cartItems =[
     {
-        id: 'International',
+        id: 1,
         product: 'Chili',
         price: 27.00,
         quantity: 15,
     },
 
     {
-        id:'Produce',
+        id:2,
         product: 'Bananas',
         price:80.00,
         quantity: 1,
     },
 
     {
-        id: "Specialty",
+        id: 3,
         product:'Wine',
         price:100.00,
         quantity:1,
     },
 
     {   
-        id: 'Grocery',
+        id: 4,
         product: 'Cookies',
         price: 79.00,
         quantity: 5,
 
     },
 ];
-router.get('/cart-items', (req, res) => {
-    res.send(cartItems[0])
+
+
+cart.get('/cart-items', (req, res) => {
+    for(carts of cartItems);
+    res.json(carts[0]);
+    
 });
 
-router.post("/cart-items", (req, res) => {
-    console.log(req.body)
-    cartItems.push({
-        id: cartItems, length + 1,
-        product: req.body.product,
-        price:req.body.price,
-        quantity: req.body.quantity,
-    })
-})
+module.exports = cart;
 
-router.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
-});
+app.listen(3000, () => console.log(`Cart app is listening to ${port} the hard way. If you know what I mean.`));
